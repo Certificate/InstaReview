6c250a12f746265b5abc7f8fc3fa92c011a0f84c
Defrosted <ermurj@hotmail.com>
2018-09-30 15:48:34 +0300

    Set up Docker for the Node.js project.

--- API/MobileAPI/app.js
+++ API/MobileAPI/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 80;
+const host = '0.0.0.0';
 
 app.get('/HelloWorld', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`App running on port ${port}`));
+app.listen(port, host, () => console.log(`App running on port http://${host}:${port}`));
