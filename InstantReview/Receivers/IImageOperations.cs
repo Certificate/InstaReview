adc9001789394fb4177909f35bc9156dc83ba31b
Valtteri Vuori <valtteri.vuori@live.fi>
2018-10-23 21:01:58 +0300

    Moved file to another project

---
+++ InstantReview/Receivers/IImageOperations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace InstantReview.Droid
+{
+    public interface IImageOperations
+    {
+        event EventHandler<EventArgs> ImageReceivedEvent;
+
+        Image UserImage { get; set; }
+    }
+}
