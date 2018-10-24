9e0f24ae175154f906772fa44c063ba84e849388
Valtteri Vuori <valtteri.vuori@live.fi>
2018-10-24 23:39:05 +0300

    Added tool to parse uri's and added permissions for external storage

--- InstantReview.Android/ContainerCreator.cs
+++ InstantReview.Android/ContainerCreator.cs
@@ -13,6 +13,7 @@ namespace InstantReview.Droid
         {
             var builder = new ContainerBuilder();
             builder.RegisterType<DialogService>().As<IDialogService>().SingleInstance();
+            builder.RegisterType<UriTool>().AsSelf().SingleInstance();
             builder.RegisterType<ShareIntentReceiver>().As<IShareIntentReceiver>().SingleInstance();
             builder.RegisterInstance(myReceiver).AsImplementedInterfaces();
 
