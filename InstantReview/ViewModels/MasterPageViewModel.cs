71e4913c36482b8e329fdf90105dfec3597e70fa
Valtteri Vuori <valtteri.vuori@live.fi>
2018-11-08 21:32:09 +0200

    Changes to masterpage

--- InstantReview/ViewModels/MasterPageViewModel.cs
+++ InstantReview/ViewModels/MasterPageViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Windows.Input;
 using Common.Logging;
+using InstantReview.Login;
 using Xamarin.Forms;
 
 namespace InstantReview.ViewModels
@@ -8,11 +10,22 @@ namespace InstantReview.ViewModels
     {
         private static readonly ILog Log = LogManager.GetLogger<MasterPageViewModel>();
 
+        public event EventHandler<EventArgs> LogoutSuccessful;
+
+        private readonly ILoginHandler login;
+
+        public MasterPageViewModel(ILoginHandler login)
+        {
+            this.login = login;
+        }
+
         public ICommand LogOutCommand => new Command(LogOut);
 
         private void LogOut()
         {
+            login.DeletePrivileges();
             Log.Debug("Log Out!");
+            LogoutSuccessful?.Invoke(this, EventArgs.Empty);
         }
     }
 }
