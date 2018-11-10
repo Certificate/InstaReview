53e5e1ff7c26d336526cf22b3d7336419dd3792a
Kirsi <rajamaki.kirsi.m@student.uta.fi>
2018-11-10 17:02:45 +0200

    Swiping Instructions Page
    New implementation branch

---
+++ InstantReview/Views/SwipingInstrutionsPage.xaml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace InstantReview
+{
+    public partial class SwipingInstructionsPage : ContentPage
+    {
+        public SwipingInstructionsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
