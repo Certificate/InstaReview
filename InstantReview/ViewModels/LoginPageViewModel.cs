f6dbc32866749e13667b0e2bed35fc35b5cea073
Valtteri Vuori <valtteri.vuori@live.fi>
2018-12-06 02:54:17 +0200

    Moved register functionality from LoginPageViewModel to RegisterPageViewModel

--- InstantReview/ViewModels/LoginPageViewModel.cs
+++ InstantReview/ViewModels/LoginPageViewModel.cs
@@ -3,7 +3,6 @@ using System.Windows.Input;
 using Common.Logging;
 using InstantReview.Droid;
 using InstantReview.Login;
-using System.Net.Http;
 using System.Text;
 using System.Threading;
 using Newtonsoft.Json;
@@ -18,10 +17,10 @@ namespace InstantReview.ViewModels
 {
     public class LoginPageViewModel : BaseViewModel ,ILoginPageViewModel
     {
+        private static readonly ILog Log = LogManager.GetLogger<LoginPageViewModel>();
+
         public event EventHandler<EventArgs> LoginSuccessful;
         private readonly IDialogService dialogService;
-        private static readonly ILog Log = LogManager.GetLogger<LoginPageViewModel>();
-        private static readonly HttpClient client = new HttpClient();
         private readonly IConnectionHandler _connectionHandler;
         private readonly INavigation navigation;
         private readonly IPageFactory pageFactory;
@@ -34,6 +33,12 @@ namespace InstantReview.ViewModels
             this.navigation = navigation;
             this.pageFactory = pageFactory;
             this.registerPageViewModel = registerPageViewModel;
+            registerPageViewModel.RegisterSuccessful += RegisterPageViewModelOnRegisterSuccessful;
+        }
+
+        private void RegisterPageViewModelOnRegisterSuccessful(object sender, EventArgs e)
+        {
+            LoginSuccessful?.Invoke(this, EventArgs.Empty);
         }
 
         public ICommand LoginCommand => new Command(StartLoginProcess);
@@ -50,53 +55,12 @@ namespace InstantReview.ViewModels
             return pageFactory.CreatePage<RegisterPage, RegisterPageViewModel>(registerPageViewModel);
         }
 
-        private async void RegisterUser()
-        {
-            var success = false;
-            try
-            {
-                var response = await _connectionHandler.Register(Username, password);
-                if (response.StatusCode != HttpStatusCode.OK)
-                {
-                    throw new Exception("Response was not OK. Aborting.");
-                }
-
-                var token = JsonConvert.DeserializeObject<Response>(await response.Content.ReadAsStringAsync());
-
-                var isActive = _connectionHandler.CheckTokenValidity(token.token);
-
-                if (isActive)
-                {
-                    _connectionHandler.SaveUsagePrivileges(token.token);
-                    success = true;
-                }
-            }
-            catch (Exception e)
-            {
-                Log.Error("Error while logging in!", e);
-            }
-            finally
-            {
-                Password = string.Empty;
-                OnPropertyChanged(nameof(Password));
-            }
-
-            if (success)
-            {
-                Log.Debug("Logged in successfully!");
-                Username = string.Empty;
-                OnPropertyChanged(nameof(Username));
-                dialogService.showRegisteredDialog();
-                LoginSuccessful?.Invoke(this, EventArgs.Empty);
-            }
-        }
-
         private async void StartLoginProcess()
         {
             var success = false;
             try
             {
-                var response = await _connectionHandler.Login(Username, password);
+                var response = await _connectionHandler.Login(Username, Password);
                 if (response.StatusCode != HttpStatusCode.OK)
                 {
                     throw new Exception("Response was not OK. Aborting.");
@@ -139,26 +103,8 @@ namespace InstantReview.ViewModels
             public string token;
         }
 
-        private string username;
+        public string Username { get; set; }
 
-        public string Username
-        {
-            get { return username; }
-            set
-            {
-                username = value;
-            }
-        }
-
-        private string password;
-
-        public string Password
-        {
-            get { return password; }
-            set
-            {
-                password = value;
-            }
-        }
+        public string Password { get; set; }
     }
 }
--- InstantReview/ViewModels/RegisterPageViewModel.cs
+++ InstantReview/ViewModels/RegisterPageViewModel.cs
@@ -1,7 +1,95 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Windows.Input;
+using Common.Logging;
+using InstantReview.Login;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
 namespace InstantReview.ViewModels
 {
-    public class RegisterPageViewModel
+    public class RegisterPageViewModel : BaseViewModel
     {
-        
+        private static readonly ILog Log = LogManager.GetLogger<RegisterPageViewModel>();
+
+        public event EventHandler<EventArgs> RegisterSuccessful;
+        private readonly IDialogService dialogService;
+        private readonly IConnectionHandler connectionHandler;
+        private readonly INavigation navigation;
+        private readonly IPageFactory pageFactory;
+
+        public RegisterPageViewModel(IDialogService dialogService, IConnectionHandler connectionHandler, INavigation navigation, IPageFactory pageFactory)
+        {
+            this.dialogService = dialogService;
+            this.connectionHandler = connectionHandler;
+            this.navigation = navigation;
+            this.pageFactory = pageFactory;
+        }
+
+        public ICommand RegisterCommand => new Command(RegisterUser);
+
+        private async void RegisterUser()
+        {
+            var success = false;
+            try
+            {
+                if (!Password.Equals(CheckPassword))
+                {
+                    throw new ArgumentException("Passwords do not match");
+                }
+
+                var response = await connectionHandler.Register(Email, Password);
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new Exception("Response was not OK. Aborting.");
+                }
+
+                var token = JsonConvert.DeserializeObject<Response>(await response.Content.ReadAsStringAsync());
+
+                var isActive = connectionHandler.CheckTokenValidity(token.token);
+
+                if (isActive)
+                {
+                    connectionHandler.SaveUsagePrivileges(token.token);
+                    success = true;
+                }
+            }
+            catch (ArgumentException e)
+            {
+                Log.Error("Password mismatch!", e);
+            }
+            catch (Exception e)
+            {
+                Log.Error("Error while logging in!", e);
+            }
+            finally
+            {
+                Password = string.Empty;
+                OnPropertyChanged(nameof(Password));
+                CheckPassword = string.Empty;
+                OnPropertyChanged(nameof(CheckPassword));
+            }
+
+            if (success)
+            {
+                Log.Debug("Logged in successfully!");
+                Email = string.Empty;
+                OnPropertyChanged(nameof(Email));
+                dialogService.showRegisteredDialog();
+                RegisterSuccessful?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        protected class Response
+        {
+            public string token;
+        }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string CheckPassword { get; set; }
     }
 }
