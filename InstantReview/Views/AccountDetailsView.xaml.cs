a29afab787038e6d3dea2cfdd2518211dcdc6e9b
Valtteri Vuori <valtteri.vuori@live.fi>
2018-12-09 17:05:42 +0200

    Base code for edit account details. Conflict with two layouts, must fix

---
+++ InstantReview/Views/AccountDetailsView.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace InstantReview.Views
+{
+    public partial class AccountDetailsView : ContentPage
+    {
+        public AccountDetailsView()
+        {
+            InitializeComponent();
+        }
+
+        private void PasswordTextChanged(object sender, PropertyChangedEventArgs e)
+        {
+            try
+            {
+                if (password.Text != null && checkPassword.Text != null &&!password.Text.Equals(checkPassword.Text))
+                {
+                    password.TextColor = Color.Red;
+                    checkPassword.TextColor = Color.Red;
+                    passwordErrorText.IsVisible = true;
+                }
+                else
+                {
+                    password.TextColor = Color.Default;
+                    checkPassword.TextColor = Color.Default;
+                    passwordErrorText.IsVisible = false;
+                }
+                CheckButtonState();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
+        }
+
+        private void CheckButtonState()
+        {
+            var isValidEmail = false;
+            
+            // Regex for valid email address
+            var regex = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+            if (!string.IsNullOrEmpty(email.Text))
+            {
+                isValidEmail = regex.IsMatch(email.Text);
+            }
+
+            if (!string.IsNullOrEmpty(email.Text) && 
+                password.Text != null && 
+                checkPassword.Text != null && 
+                password.Text.Equals(checkPassword.Text) &&
+                isValidEmail)
+            {
+                registerButton.IsEnabled = true;
+                registerButton.BorderColor = Color.Salmon;
+            }
+            else
+            {
+                registerButton.IsEnabled = false;
+                registerButton.BorderColor = Color.Default;
+            }
+        }
+
+        private void EmailTextChangedEvent(object sender, PropertyChangedEventArgs e)
+        {
+            CheckButtonState();
+            
+        }
+    }
+}
